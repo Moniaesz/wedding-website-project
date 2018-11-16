4775a5ac64527cb31cf536a983366471d1b935d4
Monika <molinskamonika+github@gmail.com>
2018-11-16 11:23:29 +0100

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync');
+
+gulp.task('sass', function() {
+    return gulp.src('src/sass/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('src/css'))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('serve', function() {
+
+    browserSync.init({
+        server: 'src/'
+    });
+});
+
+gulp.task('watch', function() {
+
+    gulp.watch('src/sass/*.scss', ['sass']);
+    gulp.watch(['src/*.html', 'src/css/*.css'], browserSync.reload);
+});
+
+gulp.task('default', ['sass', 'serve', 'watch']);
