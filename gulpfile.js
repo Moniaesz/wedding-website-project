d92a76fefe778546c46c85541bf60b99213189f8
Monika <molinskamonika+github@gmail.com>
2018-11-29 09:13:07 +0100

    gulp build task

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const browserSync = require('browser-sync');
+const runSequence = require('run-sequence');
+const del = require('del');
 
 gulp.task('sass', function() {
     return gulp.src('src/sass/*.scss')
@@ -21,5 +23,29 @@ gulp.task('watch', function() {
     gulp.watch('src/sass/*.scss', ['sass']);
     gulp.watch(['src/*.html', 'src/css/*.css', 'src/js/app.js'], browserSync.reload);
 });
+
+gulp.task('clean', function() {
+
+    return del('dist/');
+
+});
+
+gulp.task('copy', function() {
+
+    return gulp.src([ 'src/*.html', 'src/css/**/*.css', 'src/assets/**/', 'src/**/*.js'], {
+
+        base: 'src'
+    })
+
+    .pipe(gulp.dest('dist/'));
+
+});
+
+gulp.task('build', function(callback) {
+
+    runSequence('clean', 'copy', callback);
+
+});
+
 
 gulp.task('default', ['sass', 'serve', 'watch']);
